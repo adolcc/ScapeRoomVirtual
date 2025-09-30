a213817281664f2064d59a7db4c3ae688d44514e
Lucy C. <you@example.com>
2025-09-30 19:02:11 +0200

    renaming packages and DAO

--- src/main/java/exception/DuplicateEscapeRoomNameException.java
+++ src/main/java/exception/DuplicateEscapeRoomNameException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package exception;
 
 public class DuplicateEscapeRoomNameException extends RuntimeException {
 
--- src/main/java/exception/NullEscapeRoomNameException.java
+++ src/main/java/exception/NullEscapeRoomNameException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package exception;
 
 public class NullEscapeRoomNameException extends RuntimeException {
 
