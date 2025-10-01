95ce099b94045606846281a011481a85a3929857
Lucy C. <you@example.com>
2025-10-01 18:11:06 +0200

    updated ERService to use DAO & ER to validate name in constructor

--- src/main/java/model/EscapeRoom.java
+++ src/main/java/model/EscapeRoom.java
@@ -1,5 +1,8 @@
 package model;
 
+import exception.EmptyEscapeRoomNameException;
+import exception.NullEscapeRoomNameException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -11,25 +14,35 @@ public class EscapeRoom {
     private List<Room> rooms;
 
     public EscapeRoom(String name) {
+        validateName(name);
+
         this.name = name;
         this.rooms = new ArrayList<>();
         this.id = null;
     }
 
+    private void validateName(String name) {
+        if (name == null) {
+            throw new NullEscapeRoomNameException();
+        }
+        if (name.trim().isEmpty()) {
+            throw new EmptyEscapeRoomNameException();
+        }
+    }
+
     public String getName() {
         return this.name;
     }
-    public Long getId() { return this.id; }
     public List<Room> getRooms() {
         return rooms;
     }
+    public Long getId() { return this.id; }
 
     public void setName(String name) { this.name = name; }
-    public void setId(Long id) { this.id = id; }
-
     public void addRoom(Room room) {
         rooms.add(room);
     }
+    public void setId(Long id) { this.id = id; }
 
     @Override
     public boolean equals(Object o) {
