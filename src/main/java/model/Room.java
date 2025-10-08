3b4f088819543e3c03a6bc1c920c15d626b398e2
carlosPc1987 <cadecozarrus@gmail.com>
2025-10-08 16:47:10 +0200

    feat: add Room entity with id and basic properties

--- src/main/java/model/Room.java
+++ src/main/java/model/Room.java
@@ -11,6 +11,8 @@ public class Room {
     private int level;
     private List<Clue> clues;
     private List<Decoration> decorations;
+    private long id;
+
 
     public Room(String name, int level) {
         if (name == null || name.trim().isEmpty()) {
@@ -20,6 +22,7 @@ public class Room {
         this.level = level;
         this.clues = new ArrayList<>();
         this.decorations = new ArrayList<>();
+        this.name = name;
     }
 
     public String getName() {
@@ -46,6 +49,16 @@ public class Room {
         decorations.add(decoration);
     }
 
+    public void setId(long id){
+        this.id=id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+     public void setName (String name){
+        this.name = name;
+     }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
