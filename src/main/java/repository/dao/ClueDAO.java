992136821597cfb009ae263d17086bb1ddc71c87
carlosPc1987 <cadecozarrus@gmail.com>
2025-10-17 09:51:33 +0200

    cambios de clues y roomservice

--- src/main/java/repository/dao/ClueDAO.java
+++ src/main/java/repository/dao/ClueDAO.java
@@ -23,7 +23,7 @@ public class ClueDAO implements GenericDAO<Clue, Long> {
             throw new IllegalArgumentException("El precio de la pista debe ser mayor que cero");
         }
 
-        if (entity.getEscapeRoom() == null) {
+        if (entity.getRoomId() == null) {
             throw new IllegalArgumentException("La pista debe estar asociada a una sala");
         }
 
