69e14c253626562f07f3b4e1e033f16ad8e1672b
Lucy C. <you@example.com>
2025-10-21 13:20:49 +0200

    eddit Player to compare emails

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,6 +1,6 @@
 package model;
 
-import java.util.UUID;
+import java.util.Objects;
 
 public class Player {
     private String id;
@@ -10,7 +10,7 @@ public class Player {
 
     public Player(String name, String email) {
         validateEmail(email);
-        this.id = UUID.randomUUID().toString();
+        this.id = null;
         this.name = name;
         this.email = email;
         this.newsletterSubscribed = false;
@@ -23,11 +23,11 @@ public class Player {
     }
 
     public String getId() {
-        return id;
+        return this.id;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public void setName(String name) {
@@ -35,7 +35,7 @@ public class Player {
     }
 
     public String getEmail() {
-        return email;
+        return this.email;
     }
 
     public void setEmail(String email) {
@@ -44,10 +44,24 @@ public class Player {
     }
 
     public boolean isNewsletterSubscribed() {
-        return newsletterSubscribed;
+        return this.newsletterSubscribed;
     }
 
     public void setNewsletterSubscribed(boolean newsletterSubscribed) {
         this.newsletterSubscribed = newsletterSubscribed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player other = (Player) o;
+
+        return this.email.equalsIgnoreCase(other.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.email != null ? this.email.toLowerCase() : null);
     }
 }
